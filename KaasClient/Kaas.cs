c4f45181a513f54c8b50a039beacf2e99cdd170a
Stijn Vancoppenolle <stijn.vcpn@gmail.com>
2021-05-26 09:36:31 +0200

    C# KaasClient

---
+++ KaasClient/Kaas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace KaasClient.Models {
+    [DataContract(Name = "kaas", Namespace = "")]
+    public class Kaas {
+        [DataMember(Name = "id", Order = 1)]
+        public int Id { get; set; }
+        [DataMember(Name = "naam", Order = 2)]
+        public string Naam { get; set; }
+        [DataMember(Name = "soort", Order = 3)]
+        public string Soort { get; set; }
+        [DataMember(Name = "smaak", Order = 4)]
+        public string Smaak { get; set; }
+    }
+}
+
---
+++ KaasClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using KaasClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KaasClient {
+    class Program {
+        static async Task Main() {
+            Console.Write("Smaak:");
+            var smaak = Console.ReadLine();
+            using var client = new HttpClient();
+            var response =
+            await client.GetAsync($"http://localhost:5000/kazen/smaken?smaak={smaak}");
+            switch (response.StatusCode) {
+                case HttpStatusCode.OK:
+                    var kazen = await response.Content.ReadAsAsync<List<Kaas>>();
+                    kazen.ForEach(kaas => Console.WriteLine(kaas.Naam));
+                    break;
+                case HttpStatusCode.NotFound:
+                    Console.WriteLine("Kazen niet gevonden");
+                    break;
+                default:
+                    Console.WriteLine("Technisch probleem, contacteer de helpdesk.");
+                    break;
+            }
+        }
+    }
+}
